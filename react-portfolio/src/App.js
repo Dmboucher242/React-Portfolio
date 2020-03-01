a4fff9d9ebfa2256ebd14d0a349fcb6a8599c3f8
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-29 21:40:58 -0700

    adding files and the navbar

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from "./components/Nav/index"
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
