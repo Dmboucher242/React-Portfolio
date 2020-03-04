5dc1a8786dc176f41869dcd116f21c5983a1e1f8
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-03-03 20:48:56 -0700

    adding routes to the different pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from "./components/Nav/index"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav/index";
+import Portfolio from "./components/Portfolio/index";
+import Contact from "./components/Contact/index";
+
+
 
 function App() {
   return (
     <div>
       <Nav />
+
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Nav />
+          </Route>
+
+          <Route exact path='/portfolio'>
+            <Portfolio />
+          </Route>
+
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
